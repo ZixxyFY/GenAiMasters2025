b03c3d7cdfca5cc54ecc072966bebea5c1109a8c
ZixxyFY <neeraj112005sahu@gmail.com>
2025-06-06 19:45:18 +0530

    Have done the Question 5

---
+++ src/DAY3/Q5.kt
@@ -0,0 +1,9 @@
+package DAY3
+
+fun main() {
+    val orderNumber = 9
+    if (orderNumber % 2 == 0)
+        println("Order No.$orderNumber is even. It's an espresso day!")
+    else
+        println("Order No.$orderNumber is odd. You would like a latte today!")
+}
