19d906ca4a5cfa735f58150b914b9c31c5d2f646
ZixxyFY <neeraj112005sahu@gmail.com>
2025-06-07 07:56:31 +0530

    Have done the Question 16

---
+++ src/DAY3/Q16.kt
@@ -0,0 +1,9 @@
+package DAY3
+
+fun main() {
+    val orderName = "Latte"
+    if (orderName.length >= 5)
+        println("Order name '$orderName' is valid!")
+    else
+        println("Order name '$orderName' is too short!")
+}
