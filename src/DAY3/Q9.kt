9dab86e9ae326c74a0c569b2c045c2b0a2076ee6
ZixxyFY <neeraj112005sahu@gmail.com>
2025-06-07 07:48:43 +0530

    Have done the Question 9

---
+++ src/DAY3/Q9.kt
@@ -0,0 +1,9 @@
+package DAY3
+
+fun main() {
+    val age = 61
+    if (age >= 60)
+        println("Eligible for the senior coffee discount, Enjoy!")
+    else
+        println("Sorry, no discount yet!")
+}
