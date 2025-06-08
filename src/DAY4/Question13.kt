78a8ca4dea231bf1b340b35afd718c8fca90de6d
ZixxyFY <neeraj112005sahu@gmail.com>
2025-06-08 19:28:41 +0530

    Have done the Question 13

---
+++ src/DAY4/Question13.kt
@@ -0,0 +1,9 @@
+package DAY4
+
+fun countUniqueOrders(orders: Set<String>) {
+    println("Unique orders: ${orders.size}")
+}
+fun main() {
+    val orders = setOf("Latte", "Espresso", "Latte")
+    countUniqueOrders(orders)
+}
