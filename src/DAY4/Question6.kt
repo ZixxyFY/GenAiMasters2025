a97d50c51428a89d93df6d315131c2336d4e1972
ZixxyFY <neeraj112005sahu@gmail.com>
2025-06-08 14:58:00 +0530

    Have done the Question 6

---
+++ src/DAY4/Question6.kt
@@ -0,0 +1,10 @@
+package DAY4
+
+fun highestPrice(prices: List<Double>): Double? {
+    return prices.maxOrNull()
+}
+
+fun main() {
+    val prices = listOf(120.0, 150.0, 90.0)
+    println("Highest price: ₹${highestPrice(prices)}")
+}
