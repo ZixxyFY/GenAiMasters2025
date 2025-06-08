681342f5e3044a34dc7c5cdf541434faef39b0a1
ZixxyFY <neeraj112005sahu@gmail.com>
2025-06-08 15:05:13 +0530

    Have done the Question 10

---
+++ src/DAY4/Question10.kt
@@ -0,0 +1,10 @@
+package DAY4
+
+class CoffeeCup(val temperature: Int) {
+    fun isTooHot(): Boolean = temperature > 70
+}
+
+fun main() {
+    val cup = CoffeeCup(75)
+    println("Is the coffee too hot? ${cup.isTooHot()}")
+}
