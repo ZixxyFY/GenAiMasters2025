cb5d0534e1509adf2ee950dd3da4117032adfa56
ZixxyFY <neeraj112005sahu@gmail.com>
2025-06-09 20:09:44 +0530

    Have done the Question 6

---
+++ src/DAY5/Q6.kt
@@ -0,0 +1,10 @@
+package DAY5
+
+fun main() {
+    try {
+        val number = "abc".toInt()
+        println(number)
+    } catch (e: NumberFormatException) {
+        println("Invalid number format")
+    }
+}
