fd49a92ae76393b967ffccf00b498a6f1a0d9c82
ZixxyFY <neeraj112005sahu@gmail.com>
2025-06-09 20:06:19 +0530

    Have done the Question 4

---
+++ src/DAY5/Q4.kt
@@ -0,0 +1,8 @@
+package DAY5
+
+fun main() {
+    val names = listOf("Alice", "Bob", "Charlie")
+    val uppercased = names.map { it.uppercase() }
+    println("Map: $uppercased")
+    names.forEach { println("Hello, $it") }
+}
